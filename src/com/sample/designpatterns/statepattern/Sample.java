6a1d8b8a89a4a52d16702e75522850d36c4a446c
venkatasivaram <pvsivarammca@gmail.com>
2016-07-06 21:40:56 +0530

    first commit

---
+++ src/com/sample/designpatterns/statepattern/Sample.java
@@ -0,0 +1,10 @@
+package com.sample.designpatterns.statepattern;
+
+public class Sample {
+	
+	public static void main(String args[])
+	{
+		System.out.println("hai welcome to state pattern");
+	}
+
+}
